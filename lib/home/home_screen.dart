9beec9af6ee54c3783d0980381fe2e69803300f4
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-04-09 20:39:58 +0900

    웹툰 이미지, 웹툰명, 작가, 별점 추가

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:naverwebtoon_clone/constant/breakpoints.dart';
+import 'package:naverwebtoon_clone/constant/gaps.dart';
 import 'package:naverwebtoon_clone/constant/sizes.dart';
 
 
@@ -27,36 +29,102 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: tabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-            elevation: 1,
-            title: ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxWidth: Breakpoints.sm,
-              ),
-            ),
-            centerTitle: true,
-            bottom: TabBar(
-              splashFactory: NoSplash.splashFactory,
-              padding: const EdgeInsets.symmetric(
-                horizontal: Sizes.size4,
-              ),
-              isScrollable: true,
-              labelStyle: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: Sizes.size16,
-              ),
-              indicatorColor: Theme.of(context).tabBarTheme.indicatorColor,
-              tabs: [
-                for (var tab in tabs)
-                  Tab(
-                    text: tab,
-                  ),
-              ],
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: Breakpoints.sm,
             ),
           ),
+          centerTitle: true,
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size4,
+            ),
+            isScrollable: true,
+            labelStyle: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            indicatorColor: Theme.of(context).tabBarTheme.indicatorColor,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
         ),
+        body: TabBarView(
+          children: [
+            GridView.builder(
+                itemCount: 10,
+                padding: const EdgeInsets.all(
+                  Sizes.size10,
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: Sizes.size6,
+                  mainAxisSpacing: Sizes.size6,
+                  childAspectRatio: 9 / 20,
+                ),
+                itemBuilder: (context, index) => LayoutBuilder(
+                    builder: (context, constraints) => Column(
+                          children: [
+                            Container(
+                              clipBehavior: Clip.hardEdge,
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.circular(Sizes.size4),
+                              ),
+                              child: AspectRatio(
+                                aspectRatio: 9 / 15,
+                                child: FadeInImage.assetNetwork(
+                                  fit: BoxFit.cover,
+                                  placeholder: "https://avatars.githubusercontent.com/u/121213240?v=4",
+                                  image: "https://shared-comic.pstatic.net/thumb/webtoon/736277/thumbnail/thumbnail_IMAG21_bbbe3f76-021e-4dbc-830a-4358c1abec0c.jpg",
+                                ),
+                              ),
+                            ),
+                            Gaps.v10,
+                            const Text(
+                              "싸움독학",
+                              style: TextStyle(
+                                fontSize: Sizes.size16,
+                                fontWeight: FontWeight.bold,
+                                height: 1.1
+                              ),
+                            ),
+                            Gaps.v8,
+                            Row(
+                              children: [
+                                Expanded(
+                                    child: Text(
+                                      "박태준 만화회사 / 김정현 스튜디오",
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                ),
+                                Gaps.h2,
+                                FaIcon(
+                                  FontAwesomeIcons.solidStar,
+                                  size: Sizes.size16,
+                                  color: Colors.grey.shade600,
+                                ),
+                                Gaps.h2,
+                                const Text(
+                                  "9.72",
+                                )
+                              ],
+                            )
+                          ],
+                        )))
+          ],
+        ),
+      ),
     );
   }
 }
