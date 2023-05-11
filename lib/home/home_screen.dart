fd15900021a90e57224dbfeb0584ed2fec4b8823
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-05-11 18:07:00 +0900

    PageView

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -4,7 +4,6 @@ import 'package:naverwebtoon_clone/constant/breakpoints.dart';
 import 'package:naverwebtoon_clone/constant/gaps.dart';
 import 'package:naverwebtoon_clone/constant/sizes.dart';
 
-
 final tabs = [
   "신작",
   "매일+",
@@ -18,6 +17,8 @@ final tabs = [
   "완결",
 ];
 
+final PageController pageController = PageController(initialPage: 0);
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -28,101 +29,119 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: tabs.length,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 1,
-          title: ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxWidth: Breakpoints.sm,
-            ),
-          ),
-          centerTitle: true,
-          bottom: TabBar(
-            splashFactory: NoSplash.splashFactory,
-            padding: const EdgeInsets.symmetric(
-              horizontal: Sizes.size4,
-            ),
-            isScrollable: true,
-            labelStyle: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: Sizes.size16,
-            ),
-            indicatorColor: Theme.of(context).tabBarTheme.indicatorColor,
-            tabs: [
-              for (var tab in tabs)
-                Tab(
-                  text: tab,
+    return SafeArea(
+      child: DefaultTabController(
+        length: tabs.length,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.cookie, color: Colors.amber, size: 20),
+                  ),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.search, color: Colors.white, size: 20),
+                  ),
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 200,
+                  child: PageView(
+                    scrollDirection: Axis.horizontal,
+                    controller: pageController,
+                    children: [
+                      Image.network(
+                        "https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074_960_720.jpg",
+                        fit: BoxFit.fill,
+                      ),
+                      Image.network(
+                        "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg",
+                        fit: BoxFit.fill,
+                      ),
+                      Image.network(
+                        "https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_960_720.jpg",
+                        fit: BoxFit.fill,
+                      ),
+                    ],
+                  ),
                 ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            GridView.builder(
-                itemCount: 10,
-                padding: const EdgeInsets.all(
-                  Sizes.size10,
-                ),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: Sizes.size6,
-                  mainAxisSpacing: Sizes.size6,
-                  childAspectRatio: 9 / 20,
-                ),
-                itemBuilder: (context, index) => LayoutBuilder(
-                    builder: (context, constraints) => Column(
-                          children: [
-                            Container(
-                              clipBehavior: Clip.hardEdge,
-                              decoration: BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.circular(Sizes.size4),
-                              ),
-                              child: AspectRatio(
-                                aspectRatio: 9 / 15,
-                                child: FadeInImage.assetNetwork(
-                                  fit: BoxFit.cover,
-                                  placeholder: "https://avatars.githubusercontent.com/u/121213240?v=4",
-                                  image: "https://shared-comic.pstatic.net/thumb/webtoon/736277/thumbnail/thumbnail_IMAG21_bbbe3f76-021e-4dbc-830a-4358c1abec0c.jpg",
+              )
+            ];
+          },
+          body: TabBarView(
+            children: [
+              GridView.builder(
+                  itemCount: 10,
+                  padding: const EdgeInsets.all(
+                    Sizes.size10,
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: Sizes.size6,
+                    mainAxisSpacing: Sizes.size6,
+                    childAspectRatio: 9 / 20,
+                  ),
+                  itemBuilder: (context, index) => LayoutBuilder(
+                      builder: (context, constraints) => Column(
+                            children: [
+                              Container(
+                                clipBehavior: Clip.hardEdge,
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.circular(Sizes.size4),
+                                ),
+                                child: AspectRatio(
+                                  aspectRatio: 9 / 15,
+                                  child: FadeInImage.assetNetwork(
+                                    fit: BoxFit.cover,
+                                    placeholder:
+                                        "https://avatars.githubusercontent.com/u/121213240?v=4",
+                                    image:
+                                        "https://shared-comic.pstatic.net/thumb/webtoon/736277/thumbnail/thumbnail_IMAG21_bbbe3f76-021e-4dbc-830a-4358c1abec0c.jpg",
+                                  ),
                                 ),
                               ),
-                            ),
-                            Gaps.v10,
-                            const Text(
-                              "싸움독학",
-                              style: TextStyle(
-                                fontSize: Sizes.size16,
-                                fontWeight: FontWeight.bold,
-                                height: 1.1
+                              Gaps.v10,
+                              const Text(
+                                "싸움독학",
+                                style: TextStyle(
+                                    fontSize: Sizes.size16,
+                                    fontWeight: FontWeight.bold,
+                                    height: 1.1),
                               ),
-                            ),
-                            Gaps.v8,
-                            Row(
-                              children: [
-                                Expanded(
+                              Gaps.v8,
+                              Row(
+                                children: [
+                                  const Expanded(
                                     child: Text(
                                       "박태준 만화회사 / 김정현 스튜디오",
                                       maxLines: 1,
                                       overflow: TextOverflow.ellipsis,
                                     ),
-                                ),
-                                Gaps.h2,
-                                FaIcon(
-                                  FontAwesomeIcons.solidStar,
-                                  size: Sizes.size16,
-                                  color: Colors.grey.shade600,
-                                ),
-                                Gaps.h2,
-                                const Text(
-                                  "9.72",
-                                )
-                              ],
-                            )
-                          ],
-                        )))
-          ],
+                                  ),
+                                  Gaps.h2,
+                                  FaIcon(
+                                    FontAwesomeIcons.solidStar,
+                                    size: Sizes.size16,
+                                    color: Colors.grey.shade600,
+                                  ),
+                                  Gaps.h2,
+                                  const Text(
+                                    "9.72",
+                                  )
+                                ],
+                              )
+                            ],
+                          )))
+            ],
+          ),
         ),
       ),
     );
