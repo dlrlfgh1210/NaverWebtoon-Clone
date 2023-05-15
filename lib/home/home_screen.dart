3ddde3817b3a751c9ff9d6a21eeae75df58d1886
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-05-15 17:53:05 +0900

    PersistentTabBar

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:loop_page_view/loop_page_view.dart';
 import 'package:naverwebtoon_clone/constant/gaps.dart';
 import 'package:naverwebtoon_clone/constant/sizes.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:naverwebtoon_clone/persistent_tab_bar.dart';
 
 final tabs = [
   "신작",
@@ -28,7 +28,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int currentPageIndex = 0;
-  PageController pageController = PageController();
+
   LoopPageController loopPageController = LoopPageController();
   List<String> pageImage = [
     "https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074_960_720.jpg",
@@ -106,83 +106,114 @@ class _HomeScreenState extends State<HomeScreen> {
                         ],
                       ),
                     ),
-                   DotsIndicator(
-                       dotsCount: pageImage.length,
-                   position: currentPageIndex,
-                   decorator: const DotsDecorator(
-                     color: Colors.grey,
-                     activeColor: Colors.purple
-                   ),)
+                    DotsIndicator(
+                      dotsCount: pageImage.length,
+                      position: currentPageIndex,
+                      decorator: const DotsDecorator(
+                          color: Colors.grey, activeColor: Colors.purple),
+                    )
                   ],
                 ),
               ),
+              SliverPersistentHeader(
+                delegate: PersistentTabBar(),
+                pinned: true,
+              )
             ];
           },
           body: TabBarView(
             children: [
               GridView.builder(
-                  itemCount: 10,
-                  padding: const EdgeInsets.all(
-                    Sizes.size10,
+                itemCount: 10,
+                padding: const EdgeInsets.all(
+                  Sizes.size10,
+                ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: Sizes.size6,
+                  mainAxisSpacing: Sizes.size6,
+                  childAspectRatio: 9 / 20,
+                ),
+                itemBuilder: (context, index) => LayoutBuilder(
+                  builder: (context, constraints) => Column(
+                    children: [
+                      Container(
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.size4),
+                        ),
+                        child: AspectRatio(
+                          aspectRatio: 9 / 15,
+                          child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder:
+                                "https://avatars.githubusercontent.com/u/121213240?v=4",
+                            image:
+                                "https://shared-comic.pstatic.net/thumb/webtoon/736277/thumbnail/thumbnail_IMAG21_bbbe3f76-021e-4dbc-830a-4358c1abec0c.jpg",
+                          ),
+                        ),
+                      ),
+                      Gaps.v10,
+                      const Text(
+                        "싸움독학",
+                        style: TextStyle(
+                            fontSize: Sizes.size16,
+                            fontWeight: FontWeight.bold,
+                            height: 1.1),
+                      ),
+                      Gaps.v8,
+                      Row(
+                        children: [
+                          const Expanded(
+                            child: Text(
+                              "박태준 만화회사 / 김정현 스튜디오",
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Gaps.h2,
+                          FaIcon(
+                            FontAwesomeIcons.solidStar,
+                            size: Sizes.size16,
+                            color: Colors.grey.shade600,
+                          ),
+                          Gaps.h2,
+                          const Text(
+                            "9.72",
+                          )
+                        ],
+                      ),
+                    ],
                   ),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: Sizes.size6,
-                    mainAxisSpacing: Sizes.size6,
-                    childAspectRatio: 9 / 20,
-                  ),
-                  itemBuilder: (context, index) => LayoutBuilder(
-                      builder: (context, constraints) => Column(
-                            children: [
-                              Container(
-                                clipBehavior: Clip.hardEdge,
-                                decoration: BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.circular(Sizes.size4),
-                                ),
-                                child: AspectRatio(
-                                  aspectRatio: 9 / 15,
-                                  child: FadeInImage.assetNetwork(
-                                    fit: BoxFit.cover,
-                                    placeholder:
-                                        "https://avatars.githubusercontent.com/u/121213240?v=4",
-                                    image:
-                                        "https://shared-comic.pstatic.net/thumb/webtoon/736277/thumbnail/thumbnail_IMAG21_bbbe3f76-021e-4dbc-830a-4358c1abec0c.jpg",
-                                  ),
-                                ),
-                              ),
-                              Gaps.v10,
-                              const Text(
-                                "싸움독학",
-                                style: TextStyle(
-                                    fontSize: Sizes.size16,
-                                    fontWeight: FontWeight.bold,
-                                    height: 1.1),
-                              ),
-                              Gaps.v8,
-                              Row(
-                                children: [
-                                  const Expanded(
-                                    child: Text(
-                                      "박태준 만화회사 / 김정현 스튜디오",
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  Gaps.h2,
-                                  FaIcon(
-                                    FontAwesomeIcons.solidStar,
-                                    size: Sizes.size16,
-                                    color: Colors.grey.shade600,
-                                  ),
-                                  Gaps.h2,
-                                  const Text(
-                                    "9.72",
-                                  )
-                                ],
-                              )
-                            ],
-                          )))
+                ),
+              ),
+              const Center(
+                child: Text('매일+'),
+              ),
+              const Center(
+                child: Text('월요일'),
+              ),
+              const Center(
+                child: Text('화요일'),
+              ),
+              const Center(
+                child: Text('수요일'),
+              ),
+              const Center(
+                child: Text('목요일'),
+              ),
+              const Center(
+                child: Text('금요일'),
+              ),
+              const Center(
+                child: Text('토요일'),
+              ),
+              const Center(
+                child: Text('일요일'),
+              ),
+              const Center(
+                child: Text('완결'),
+              ),
             ],
           ),
         ),
---
+++ lib/persistent_tab_bar.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:naverwebtoon_clone/constant/sizes.dart';
+
+/*final tabs = [
+  "신작",
+  "매일+",
+  "월",
+  "화",
+  "수",
+  "목",
+  "금",
+  "토",
+  "일",
+  "완결",
+];*/
+
+class PersistentTabBar extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: Colors.grey.shade200,
+            width: 0.5,
+          ),
+        ),
+      ),
+      child: const TabBar(
+        isScrollable: true,
+        splashFactory: NoSplash.splashFactory,
+        indicatorSize: TabBarIndicatorSize.label,
+        indicatorColor: Colors.green,
+        labelPadding: EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        labelColor: Colors.green,
+        unselectedLabelColor: Colors.black,
+        tabs: [
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('신작'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('매일+'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('월'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('화'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('수'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('목'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('금'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('토'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('일'),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size20,
+            ),
+            child: Text('완결'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 43;
+
+  @override
+  double get minExtent => 43;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
