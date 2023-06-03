ac996bef012be8adc4887848c211be766cd0f562
dlrlfgh1210 <dlrlfgh517@gmail.com>
2023-06-03 20:28:13 +0900

    KakaoPageDetailScreen Refactoring

--- lib/recommend/kakao_page_detail_screen.dart
+++ lib/recommend/kakao_page_detail_screen.dart
@@ -65,155 +65,116 @@ class _KakaoPageDetailScreenState extends State<KakaoPageDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.purple,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Text(
-                widget.title,
-                style: const TextStyle(
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            const Icon(Icons.more_vert),
-          ],
-        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.white,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Image.network(
+            widget.img,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.black.withOpacity(0.4),
+            colorBlendMode: BlendMode.darken,
+          ),
+          Positioned(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 300,
+                bottom: 10,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Hero(
-                    tag: widget.webtoonId,
-                    child: Container(
-                      width: 300,
-                      clipBehavior: Clip.hardEdge,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 20,
-                            offset: const Offset(10, 10),
-                            color: Colors.black.withOpacity(0.5),
-                          )
-                        ],
-                      ),
-                      child: Image.network(widget.img),
-                    ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Text(widget.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                        ),),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              FutureBuilder(
-                future: details,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Column(
+                  const SizedBox(height: 20),
+                  FutureBuilder(
+                      future: details,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(
-                                '제목: ${widget.title}',
-                                style: const TextStyle(fontSize: 16,
+                              const SizedBox(height: 15),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: Text(
+                                  widget.author,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400,
+                                  ),
                                 ),
                               ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                '작가: ${widget.author}',
-                                style: const TextStyle(
-                                    fontSize: 16,),
-                                maxLines: 2,
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
                             ],
-                          ),
-                        ),
-                        IconButton(
-                          iconSize: 30,
-                          color: isLiked ? Colors.red : Colors.grey,
-                          onPressed: onHeartTap,
-                          icon: Icon(
-                            isLiked ? Icons.favorite : Icons.favorite_outline,
-                          ),
-                        ),
-                      ],
-                    );
-                  }
-                  return const Text("...");
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 1,
-                width: double.maxFinite,
-                color: Colors.grey,
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              GestureDetector(
-                onTap: () {
-                  launchUrl(
-                    Uri.parse(
-                      'https://page.kakao.com/content/${widget.webtoonId.toString().substring(5, 13)}',
-                    ),
-                  );
-                },
-                child: Container(
-                  height: 50,
-                  width: 300,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.purple,
+                          );
+                        }
+                        return const Text("...");
+                      }),
+                  const SizedBox(
+                    height: 350,
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 20,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '보러 가기',
-                          style: TextStyle(
+                  GestureDetector(
+                    onTap: () {
+                      launchUrl(
+                        Uri.parse(
+                          'https://page.kakao.com/content/${widget.webtoonId.toString().substring(5, 13)}',
+                        ),
+                      );
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.purple,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 20,
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '보러 가기',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Icon(
+                            Icons.chevron_right_rounded,
                             color: Colors.white,
-                            fontSize: 16,
                           ),
-                        ),
-                        Icon(
-                          Icons.chevron_right_rounded,
-                          color: Colors.white,
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ),
+                  )
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ],
+      ));
   }
 }
